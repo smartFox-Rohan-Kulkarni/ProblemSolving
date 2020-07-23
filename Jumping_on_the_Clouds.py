b387a2f1c296c078e078654cff9b7763861a497e
Rohan Kulkarni <57295779+smartFox-Rohan-Kulkarni@users.noreply.github.com>
2020-07-23 23:58:23 +0530

    Create Jumping_on_the_Clouds.py

---
+++ Jumping_on_the_Clouds.py
@@ -0,0 +1,66 @@
+"""
+Problem description
+Emma is playing a new mobile game that starts with consecutively numbered clouds. Some of the clouds are thunderheads and others are cumulus. She can jump on any cumulus cloud having a number that is equal to the number of the current cloud plus  or . She must avoid the thunderheads. Determine the minimum number of jumps it will take Emma to jump from her starting postion to the last cloud. It is always possible to win the game.
+
+For each game, Emma will get an array of clouds numbered  if they are safe or  if they must be avoided. For example,  indexed from . The number on each cloud is its index in the list so she must avoid the clouds at indexes  and . She could follow the following two paths:  or . The first path takes  jumps while the second takes .
+
+Function Description
+
+Complete the jumpingOnClouds function in the editor below. It should return the minimum number of jumps required, as an integer.
+
+jumpingOnClouds has the following parameter(s):
+
+c: an array of binary integers
+Input Format
+
+The first line contains an integer , the total number of clouds. The second line contains  space-separated binary integers describing clouds  where .
+
+Constraints
+
+Output Format
+
+Print the minimum number of jumps needed to win the game.
+
+Sample Input 0
+
+7
+0 0 1 0 0 1 0
+Sample Output 0
+
+4
+"""
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the jumpingOnClouds function below.
+def jumpingOnClouds(c):
+    # -1 to avoid adding extra jump at last index
+    jump_cnt = -1
+
+    cumulus = 0
+    Thunder =1
+    cloud_index = 0
+    while cloud_index < len(c):
+        if not(cloud_index+2 >= len(c)) and c[cloud_index+2] == cumulus:
+            cloud_index += 2
+        else:
+            cloud_index +=1
+        jump_cnt += 1
+
+    return jump_cnt
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    c = list(map(int, input().rstrip().split()))
+
+    result = jumpingOnClouds(c)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
